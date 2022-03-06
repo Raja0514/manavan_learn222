9281c7e6e37c4d134f5a97b13ebfe58011958b7d
Raja0514 <rajamr1987@gmail.com>
2022-03-06 21:02:28 +0400

    logo

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import { Component } from "react/cjs/react.development";
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = { count: '' };
   }
  myClick() {
     this.setState({ count: this.state.count + 1 });
